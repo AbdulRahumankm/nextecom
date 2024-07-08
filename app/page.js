849b5bc8b28d6c378f90f7862ece8f32a2cfad31
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-08 20:41:39 +0530

    layout

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return(
     <div>
-      <h1 className="d-flex justify-content-center align-item-center vh-100 text-uppercase">Home</h1>
+      <h1 className="d-flex justify-content-center align-item-center vh-100 text-uppercase ">Home</h1>
+       
     </div>
   )
 }
